3598632338c430677791c7efb55fe76b608e2e2a
Kevin Parker <kparker@rackspace.com>
2013-05-30 00:18:45 -0500

    fix indent on index.php

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
     <title>PHP HOSTNAME</title>
   </head>
   <body>
-	<div align="center">
-    		<h1>PHP HOSTNAME</h1>
-			<h2>
-				<?php
-					echo "\$_SERVER['SERVER_NAME']: ";
-					echo $_SERVER['SERVER_NAME'];
-				?>
-			</h2>
-	</div>
+    <div align="center">
+      <h1>PHP HOSTNAME</h1>
+      <h2>
+        <?php
+          echo "\$_SERVER['SERVER_NAME']: ";
+          echo $_SERVER['SERVER_NAME'];
+        ?>
+      </h2>
+    </div>
   </body>
 </html>
